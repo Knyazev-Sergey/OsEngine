dd3bbbf87e8e6161575d7e16f9ad4eca38bdba1b
Sergey Knyazev <knyazev@bk.ru>
2024-06-09 12:09:35 +0500

    Визуал робота вер.1

---
+++ project/OsEngine/Robots/DeribitPI/DeribitPI.cs
@@ -0,0 +1,41 @@
+﻿using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.OsTrader.Panels.Attributes;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+using System.Threading;
+using System.Collections.Generic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OsEngine.Robots.DeribitPI
+{
+    [Bot("DeribitPI")]
+    public class DeribitPI : BotPanel
+    {
+        private BotTabSimple _tab;
+        public BotTradeRegime Regime;
+
+        public DeribitPI(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+            Regime = BotTradeRegime.Off;
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return "DeribitPI";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+            DeribitPIUi ui = new DeribitPIUi(this);
+            ui.ShowDialog();
+        }
+    }
+}
---
+++ project/OsEngine/Robots/DeribitPI/DeribitPIUi.xaml.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
+using System.Windows;
+using OsEngine.Entity;
+using OsEngine.Language;
+using OsEngine.OsTrader.Panels;
+
+namespace OsEngine.Robots.DeribitPI
+{
+    public partial class DeribitPIUi
+    {
+        private DeribitPI _strategy;
+
+        public DeribitPIUi(DeribitPI strategy)
+        {
+            InitializeComponent();
+            OsEngine.Layout.StickyBorders.Listen(this);
+            OsEngine.Layout.StartupLocation.Start_MouseInCentre(this);
+            _strategy = strategy;
+
+            ComboBoxRegime.Items.Add(BotTradeRegime.Off);
+            ComboBoxRegime.Items.Add(BotTradeRegime.On);
+            ComboBoxRegime.SelectedItem = _strategy.Regime;
+
+           /* TextBoxVolumeOne.Text = _strategy.Volume.ToString();
+
+            ComboBoxDirection.Items.Add(Side.Buy);
+            ComboBoxDirection.Items.Add(Side.Sell);
+            ComboBoxDirection.SelectedItem = _strategy.Direction;*/
+
+            this.Activate();
+            this.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                /*if (TextBoxVolumeOne.Text.ToDecimal() <= 0)
+                {
+                    throw new Exception("");
+                }*/
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(OsLocalization.Trader.Label13);
+                return;
+            }
+
+           /* _strategy.Volume = TextBoxVolumeOne.Text.ToDecimal();
+            Enum.TryParse(ComboBoxRegime.Text, true, out _strategy.Regime);
+            Enum.TryParse(ComboBoxDirection.Text, true, out _strategy.Direction);*/
+
+            Close();
+        }
+    }
+}
